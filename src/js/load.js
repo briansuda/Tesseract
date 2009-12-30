a2c1a5bf75395e870b93878780d1b45587470f96
Matt Riggott <matt.riggott@gmail.com>
2009-12-30 13:24:34 +0000

    Replaced jQuery with XUI.

--- src/js/load.js
+++ src/js/load.js
@@ -1,13 +1,13 @@
-$(function () {
+x$(window).load(function (e) {
 	// Is the user-agent an iPhone or iPod Touch?
 	if (navigator.appVersion.indexOf('iPhone OS ') < 0) {
-		$('#loading').text('Tesseract is for the iPhone or iPod Touch only');
+		x$('#loading').inner('Tesseract is for the iPhone or iPod Touch only');
 	}
 	else {
 		// Was the game launched from the home screen?
 		if (window.navigator.standalone == undefined || window.navigator.standalone == false) {
-			$('#loading')
-				.text('Add Tesseract to your home screen to play')
+			x$('#loading')
+				.inner('Add Tesseract to your home screen to play')
 				.after('<div id=add_to_home_screen>Click ‘+’ below and then ‘Add to Home Screen’</div>');
 		}
 		else {
